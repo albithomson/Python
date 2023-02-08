2e8d2d90e78bd9ae4eb1cd68b053cf4d6598a7ee
Albi Thomson K <90432872+albithomson@users.noreply.github.com>
2023-02-08 22:44:23 +0530

    Create While loop (Print in same line).py

---
+++ While loop (Print in same line).py
@@ -0,0 +1,10 @@
+while i<=5:
+    print("Telusko ",end="")  //end is used to print in same line. 
+                              // default print will print in new line
+    j=1
+    while j<=4:
+        print("Rocks ",end="")
+        j=j+1
+
+    i=i+1
+    print()                  // to print in new line
